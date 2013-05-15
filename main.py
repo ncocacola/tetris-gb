e510920cfa885441927daad64f4adf6f1b01da7b
Nicolas Khadivi <nicolaskhadivi@gmail.com>
2013-05-15 11:17:30 +0100

    Code cleaning

--- main.py
+++ main.py
@@ -3,42 +3,21 @@ import pygame
 from pygame.locals import *
 from numpy import matrix
 
+# Helper functions ------------------------------------------------------#
+def printMatrix(matrix):
+    for row in range(len(matrix)):
+        print matrix[row]
+#------------------------------------------------------------------------#
+
+# Drawing functions -----------------------------------------------------#
 def drawGame(surface, grid):
     # grid[row][col]
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
                 pygame.draw.rect(surface, WHITE, (CELL_W*col, CELL_H*row, CELL_W, CELL_H))
-                # print "(" + str(row) + ", " + str(col) + ")"
-
-def moveLeft(piece):
-    foo = [[0]*10 for i in range(20)]
-    for row in range(len(piece)):
-        for col in range(len(piece[row])):
-            if piece[row][col] == 1:
-                foo[row][col-1] = 1
-    return foo
-def moveRight(piece):
-    foo = [[0]*10 for i in range(20)]
-    for row in range(len(piece)):
-        for col in range(len(piece[row])):
-            if piece[row][col] == 1:
-                foo[row][col+1] = 1
-    return foo
-def moveDown(piece):
-    foo = [[0]*10 for i in range(20)]
-    for row in range(len(piece)):
-        for col in range(len(piece[row])):
-            if piece[row][col] == 1:
-                foo[row+1][col] = 1
-    return foo
-
-def printMatrix(matrix):
-    for row in range(len(matrix)):
-        print matrix[row]
 
 # TODO Abstract away from drawing pieces -- pieces should be matrices?
-
 def drawIBlock(surface, color, x, y):
     pygame.draw.rect(surface, color, (x, y, CELL_W, CELL_H))
     pygame.draw.rect(surface, color, (x, y+CELL_H, CELL_W, CELL_H))
@@ -64,6 +43,31 @@ def drawLBlock(surface, color, x, y):
     pygame.draw.rect(surface, color, (x, y+CELL_H, CELL_W, CELL_H))
     pygame.draw.rect(surface, color, (x+CELL_W, y, CELL_W, CELL_H))
     pygame.draw.rect(surface, color, (x+2*CELL_W, y, CELL_W, CELL_H))
+#------------------------------------------------------------------------#
+
+# Matrix manipulation ---------------------------------------------------#
+def moveLeft(piece):
+    foo = [[0]*10 for i in range(20)]
+    for row in range(len(piece)):
+        for col in range(len(piece[row])):
+            if piece[row][col] == 1:
+                foo[row][col-1] = 1
+    return foo
+def moveRight(piece):
+    foo = [[0]*10 for i in range(20)]
+    for row in range(len(piece)):
+        for col in range(len(piece[row])):
+            if piece[row][col] == 1:
+                foo[row][col+1] = 1
+    return foo
+def moveDown(piece):
+    foo = [[0]*10 for i in range(20)]
+    for row in range(len(piece)):
+        for col in range(len(piece[row])):
+            if piece[row][col] == 1:
+                foo[row+1][col] = 1
+    return foo
+#------------------------------------------------------------------------#
 
 # Initialise the game engine
 pygame.init()
@@ -89,50 +93,28 @@ pygame.display.set_caption("Tetris")
 # Create clock
 clock = pygame.time.Clock()
 
-piece = [[0]*10 for i in range(20)]
-piece[1][1] = 1
-piece[2][1] = 1
-piece[3][1] = 1
-piece[4][1] = 1
-printMatrix(piece)
-print ""
-printMatrix(moveDown(piece))
-        # for col in range(len(grid[row])):
-        #     if grid[row][col][0] == 1:
-        #         pygame.draw.rect(surface, grid[row][col][1], (CELL_W*col, CELL_H*row, CELL_W, CELL_H))
-# print piece
-
 # Loop until quit
-# while True:
-#     # Lock the game at 50fps
-#     clock.tick(50)
+while True:
+    # Lock the game at 50fps
+    clock.tick(50)
 
-#     # Process events
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             sys.exit()
+    # Process events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-#     # Clear the screen
-#     screen.fill(BLACK)
+    # Clear the screen
+    screen.fill(BLACK)
 
-#     # Check input
+    # Check input
 
-#     # Move objects...
+    # Move objects...
 
-#     # Draw objects...
-#     grid = [[(0, BLACK)]*10 for i in range(20)]
-#     grid[19][9] = (1, BLUE)
-#     drawGame(screen, grid)
+    # Draw objects...
+    # screen.blit(background, (100,100))
 
-#     # screen.blit(background, (100,100))
-#     # drawIBlock(screen, BLUE, 0, 0)
-#     # drawOBlock(screen, RED, 0, 0)
-#     # drawZBlock(screen, WHITE, 0, 0)
-#     # drawTBlock(screen, GREEN, 0, 0)
-#     # drawLBlock(screen, RED, 0, 0)
-
-#     # Update the screen
-#     pygame.display.flip()
+    # Update the screen
+    pygame.display.flip()
 
 
 
