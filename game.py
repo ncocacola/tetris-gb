6f5b77df79134ed5a1fe72c122ef1a896e4594fe
Nicolas Khadivi <nicolaskhadivi@gmail.com>
2013-08-21 23:16:09 +0200

    Changed the 'state' to a list of tiles for games

--- game.py
+++ game.py
@@ -3,25 +3,24 @@ from config import *
 
 class Tetris(object):
     def __init__(self):
-        self.state = [[0]*ARRAY_Y for i in range(ARRAY_X)]    # 10 * 20 array of ints
+        self.tiles = []
         self.level = 1
         self.lines = 0
         self.score = 0
-
-    def merge(self, Tetronimo):
-        for col in range(len(self.state)):
-            for row in range(len(self.state[col])):
-                self.state[col][row] += Tetronimo.state[col][row]
-
-    def draw(self, surface):    
-        for col in range(len(self.state)):
-            for row in range(len(self.state[col])):
-                if self.state[col][row] == 1:
-                    pygame.draw.rect(surface, BLUE, (CELL_W*col, CELL_H*row, CELL_W, CELL_H))
-
-    def console(self):
-        # Zip an unpacked version of self.state (zip(*self.state))
+    def __repr__(self):
+        # Zip an unzipped version of self.state (zip(*self.state))
         # Apply the list() function to each resulting tuple to get a bunch of lists
-        transpose = map(list, zip(*self.state))
-        for row in range(len(transpose)):
-            print transpose[row]
+        transpose = map(list, zip(*self.get_state()))
+        return "\n".join(str(row) for row in transpose)
+    def get_state(self):
+        state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
+        for tile in self.tiles:
+            state[tile.x][tile.y] = 1
+        return state
+
+    def merge(self, block):
+        self.tiles += block.tiles
+
+    def draw(self, surface):
+        for tile in self.tiles:
+            pygame.draw.rect(surface, BLUE, (CELL_W*tile.x, CELL_H*tile.y, CELL_W, CELL_H))
