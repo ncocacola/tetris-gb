c6758279e46463a5549df6529988d34499a769ea
Nicolas Khadivi <nicolaskhadivi@gmail.com>
2013-08-21 23:15:01 +0200

    Changed the 'state' to a list of tiles for pieces

--- pieces.py
+++ pieces.py
@@ -1,142 +1,113 @@
 import pygame
 from config import *
 
+class Tile(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        return str((self.x, self.y))
+    def __eq__(self, other):
+        if isinstance(other, Tile):
+            return (self.x, self.y) == (other.x, other.y)
+        else:
+            return False
+    def __ne__(self, other):
+        return (not self.__eq__(other))
+    def __hash__(self):     # need this for comparison between Tiles in Sets
+        return hash(self.__repr__())
+
 class Tetronimo(object):
     def __init__(self):
-        self.state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
-        self.state[1][1] = 1
-        self.state[2][1] = 1
-        self.state[3][1] = 1
-        self.state[4][1] = 1
-
-    def get_coordinates(self):
-        coordinates = []
-        for row in range(len(self.state)):
-            for col in range(len(self.state[row])):
-                if self.state[row][col] == 1:
-                    coordinates.append((row, col))
-        return sorted(coordinates, key=lambda k: k[0])
-        # Sort coordinates from left to right (x-coordinate)
+        self.tiles = [Tile(1, 1), Tile(2, 1), Tile(3, 1), Tile(4,1)]
+    def __repr__(self):
+        # Zip an unzipped version of self.state (zip(*self.state))
+        # Apply the list() function to each resulting tuple to get a bunch of lists
+        transpose = map(list, zip(*self.get_state()))
+        return "\n".join(str(row) for row in transpose)
+    def get_state(self):
+        state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
+        for tile in self.tiles:
+            state[tile.x][tile.y] = 1
+        return state
 
     def can_move(self, Game, direction):
-        # TODO Only checks that piece stays in the game boundaries
-        # TODO Get it to check if other piece are not obstructing
-        coordinates = self.get_coordinates()
+        # TODO Only checks that block stays in the game boundaries
+        # TODO Get it to check if other block are not obstructing
         checklist = []
 
-        for x, y in coordinates:
-            if direction == LEFT and (x-1) > MIN_X:
-                checklist.append(True)
-            elif direction == RIGHT and (x+1) < MAX_X:
-                checklist.append(True)
-            elif direction == DOWN and (y+1) < MAX_Y:
-                checklist.append(True)
-            # TODO Remove this later
-            elif direction == UP and (y-1) > MIN_Y:
-                checklist.append(True)
+        for tile in self.tiles:
+            if direction == LEFT:
+                checklist.append(not (tile.x <= MIN_X))
+            elif direction == RIGHT:
+                checklist.append(not (tile.x >= MAX_X))
+            elif direction == DOWN:
+                checklist.append(not (tile.y >= MAX_Y))
+            elif direction == UP:
+                checklist.append(not (tile.y <= MIN_Y))
 
         return (len(checklist) == 4) and all(item == True for item in checklist)
             
     def move(self, direction):
-        coordinates = self.get_coordinates()
-        new_state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
-
-        for x, y in coordinates:
+        for tile in self.tiles:
             if direction == LEFT:
-                    new_state[x-1][y] = 1
+                tile.x -= 
             elif direction == RIGHT:
-                    new_state[x+1][y] = 1
+                tile.x += 1
+        ## Board is upside down. Down ==> Add, Up ==> Substract
             elif direction == DOWN:
-                    new_state[x][y+1] = 1
+                tile.y += 1
             # TODO Remove this later
-            elif direction == UP:
-                   new_state[x][y-1] = 1 
-                
-        self.state = new_state
+            elif direction == UP: 
+                tile.y -= 1
 
     def has_finished(self, Game):
-        coordinates = self.get_coordinates()
-
-        # If the piece has reached the bottom
-        if any(y == (MAX_Y-1) for y in [y for x, y in coordinates]):
+        # If the block has reached the bottom
+        if any(tile.y == MAX_Y for tile in self.tiles):
             return True
-        # Elif the piece has fallen on top of other blocks
-        elif any(Game.state[x][y+1] == 1 for x, y in coordinates):
+        # Elif the block has fallen on top of other blocks
+        elif list(set([Tile(tile.x, (tile.y+1)) for tile in self.tiles]) & set(Game.tiles)):
             return True
-        # Return False in the default case
         else:
             return False
 
-    def draw(self, surface):    
-        for col in range(len(self.state)):
-            for row in range(len(self.state[col])):
-                if self.state[col][row] == 1:
-                    pygame.draw.rect(surface, BLUE, (CELL_W*col, CELL_H*row, CELL_W, CELL_H))
-    
-    def console(self):
-        # Zip an unzipped version of self.state (zip(*self.state))
-        # Apply the list() function to each resulting tuple to get a bunch of lists
-        transpose = map(list, zip(*self.state))
-        for row in range(len(transpose)):
-            print transpose[row]
+        # return any(tile.y == MAX_Y for tile in self.tiles) or \
+        #        list(set([Tile(tile.x, (tile.y+1)) for tile in self.tiles]) & set(Game.tiles))
+
+    def draw(self, surface):
+        for tile in self.tiles:
+            pygame.draw.rect(surface, RED, (CELL_W*tile.x, CELL_H*tile.y, CELL_W, CELL_H))
+
+
+
 
 class TetroI(Tetronimo):
     def __init__(self):
-        self.state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
-        self.state[1][1] = 1
-        self.state[2][1] = 1
-        self.state[3][1] = 1
-        self.state[4][1] = 1
+        self.tiles = [Tile(1, 1), Tile(2, 1), Tile(3, 1), Tile(4, 1)]
 
 class TetroO(Tetronimo):
     def __init__(self):
-        self.state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
-        self.state[1][1] = 1
-        self.state[1][2] = 1
-        self.state[2][1] = 1
-        self.state[2][2] = 1
+        self.tiles = [Tile(1, 1), Tile(1, 2), Tile(2, 1), Tile(2, 2)]
 
 class TetroT(Tetronimo):
     def __init__(self):
-        self.state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
-        self.state[1][1] = 1
-        self.state[2][1] = 1
-        self.state[2][2] = 1
-        self.state[3][1] = 1
+        self.tiles = [Tile(1, 1), Tile(2, 1), Tile(2, 2), Tile(3, 1)]
 
 class TetroS(Tetronimo):
     def __init__(self):
-        self.state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
-        self.state[1][2] = 1
-        self.state[2][1] = 1
-        self.state[2][2] = 1
-        self.state[3][1] = 1
+        self.tiles = [Tile(1, 2), Tile(2, 1), Tile(2, 2), Tile(3, 1)]
 
 class TetroZ(Tetronimo):
     def __init__(self):
-        self.state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
-        self.state[1][1] = 1
-        self.state[2][1] = 1
-        self.state[2][2] = 1
-        self.state[3][2] = 1
+        self.tiles = [Tile(1, 1), Tile(2, 1), Tile(2, 2), Tile(3, 2)]
 
 class TetroJ(Tetronimo):
     def __init__(self):
-        self.state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
-        self.state[1][1] = 1
-        self.state[1][2] = 1
-        self.state[2][2] = 1
-        self.state[3][2] = 1
+        self.tiles = [Tile(1, 1), Tile(1, 2), Tile(2, 2), Tile(3, 2)]
 
 class TetroL(Tetronimo):
     def __init__(self):
-        self.state = [[0]*ARRAY_Y for i in range(ARRAY_X)]
-        self.state[3][1] = 1
-        self.state[1][2] = 1
-        self.state[2][2] = 1
-        self.state[3][2] = 1
-
-
+        self.tiles = [Tile(3, 1), Tile(1, 2), Tile(2, 2), Tile(3, 2)]
 
 
 
